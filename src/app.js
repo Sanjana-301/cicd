ec47129499b2baef747036b6bc155d6eb396ab9c
Sanjana-301 <sanjana4047@gmail.com>
2025-11-17 12:59:37 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -3,4 +3,5 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => res.send('Hello from AWS CI/CD Pipeline!'));
+app.get('/', (req, res) => res.send('Hello'));
 app.listen(port, () => console.log('App running on port ${port}'));
